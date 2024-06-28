543decbb948d6d04064d9a360f3223edf5c30eb8
obidjonjoraboyev <obidjonn007@icloud.com>
2024-06-28 14:25:06 +0500

    initial

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -7,10 +7,6 @@ class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color c010A27 = Color(0xFF010A27);
   static const Color cF07448 = CupertinoColors.systemOrange;
-  static const Color c000645 = Color(0xFF000645);
-  static const Color c252525 = Color(0xFF252525);
-  static const Color cF9F2E2 = Color(0xFFF9F2E2);
-  static const Color cB3261E = Color(0xFFB3261E);
   static const Color c4B4B4B = Color(0xFF4B4B4B);
   static const Color c2A3256 = Color(0xFF2A3256);
   static const Color c1CB0F6 = Color(0xFF1CB0F6);
--- lib/utils/app_text_style.dart
+++ lib/utils/app_text_style.dart
@@ -44,42 +44,4 @@ class AppTextStyle {
     color: AppColors.black,
   );
 
-  static TextStyle seoulPollerOneThin = const TextStyle(
-    fontFamily: "PollerOne",
-    fontWeight: FontWeight.w200,
-    color: AppColors.black,
-  );
-  static TextStyle seoulPollerOneLight = const TextStyle(
-    fontFamily: "PollerOne",
-    fontWeight: FontWeight.w300,
-    color: AppColors.black,
-  );
-  static TextStyle seoulPollerOneRegular = const TextStyle(
-    fontFamily: "PollerOne",
-    fontWeight: FontWeight.w400,
-    color: AppColors.black,
-  );
-  static TextStyle seoulPollerOneMedium = const TextStyle(
-    fontFamily: "PollerOne",
-    fontWeight: FontWeight.w500,
-    color: AppColors.black,
-  );
-
-  static TextStyle seoulPollerOneSemiBold = const TextStyle(
-    fontFamily: "PollerOne",
-    fontWeight: FontWeight.w600,
-    color: AppColors.black,
-  );
-
-  static TextStyle seoulPollerOneBold = const TextStyle(
-    fontFamily: "PollerOne",
-    fontWeight: FontWeight.w700,
-    color: AppColors.black,
-  );
-
-  static TextStyle seoulPollerOneBlack = const TextStyle(
-    fontFamily: "PollerOne",
-    fontWeight: FontWeight.w800,
-    color: AppColors.black,
-  );
 }
